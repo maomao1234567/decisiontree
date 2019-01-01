5e68a76a35f3de2d1022c8c628329036b25730a5
maomao1234567 <40746234+maomao1234567@users.noreply.github.com>
2019-01-01 16:58:34 +0800

    Count entropy (#8)

--- tree/decisiontree.py
+++ tree/decisiontree.py
@@ -1,12 +1,10 @@
 import numpy as np
 import pandas as pd
 
-
 class DecisionTree:
-    tree = [0 for i in range(900)]
+    tree = [0 for i in range(300)]
     attributes = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3', 'class']
     data_list = []
-    attributes_dict = {}
 
     def __init__(self):
         with open('../data/training_set.csv', 'r') as f:
@@ -17,10 +15,13 @@ class DecisionTree:
                 line_list = line.split(',')
                 self.data_list.append(line_list)
 
-    def get_each_attribute(self):
-        data_array = np.array(self.data_list)
-        for i in range(10):
-            self.attributes_dict[self.attributes[i]] = data_array[:, i]
+    def get_each_attribute(self, data_list, attribute_sum):
+        data_array = np.array(data_list)
+        attributes_dict = {}
+        for i in range(attribute_sum):
+            attributes_dict[self.attributes[i]] = data_array[:, i]
+
+        return attributes_dict
 
     def get_attribute_information_gain(self, attribute_array, class_array):
         attribute_martix = np.vstack((attribute_array, class_array))
@@ -67,21 +68,77 @@ class DecisionTree:
 
         return -1 * true_sum_p * np.log2(true_sum_p) + (-1) * false_sum_p * np.log2(false_sum_p)
 
-    def get_root_node(self):
-        information_gain = {}
-        for i in range(9):
-            attribute = self.attributes[i]
-            information_gain[attribute] = self.get_attribute_information_gain(self.attributes_dict[attribute], self.attributes_dict['class'])
+    def classification_by_attribute(self, dataset, attribute, value):
+        ret_data_set = []
+        for featvec in dataset:
+            if featvec[attribute] == value:
+                reducefeatvec = featvec[:attribute]
+                reducefeatvec.extend(featvec[attribute+1:])
+                ret_data_set.append(reducefeatvec)
 
+        return ret_data_set
+
+    def choose_attribute(self, attribute_dict):
         min_value = 100
-        min_key = ''
-        for key, value in information_gain.items():
+        min_attribute = ''
+        for i in range(len(self.attributes) - 1):
+            value = self.get_attribute_information_gain(attribute_dict[self.attributes[i]], attribute_dict['class'])
             if value < min_value:
                 min_value = value
-                min_key = key
+                min_attribute = self.attributes[i]
+        first_node = min_attribute
+        attribute_position = self.attributes.index(first_node)
+        self.attributes.remove(first_node)
+        return attribute_position, first_node
 
-        return min_key
+    def get_tree(self):
+        # 获取第一层的节点
+        data_set = self.data_list
+        attribute_dict = self.get_each_attribute(data_set, len(self.attributes))
+        attribute_position, first_node = self.choose_attribute(attribute_dict)
+
+        # 计算第二层的节点
+        first_node_by_x_set = self.classification_by_attribute(
+            data_set, attribute_position, 'x')
+        attribute_dict = self.get_each_attribute(first_node_by_x_set, len(self.attributes))
+        attribute_position_1, second_node_1 = self.choose_attribute(attribute_dict)
+
+        first_node_by_b_set = self.classification_by_attribute(
+            data_set, attribute_position, 'b')
+        attribute_dict = self.get_each_attribute(first_node_by_b_set, len(self.attributes))
+        attribute_position_2, second_node_2 = self.choose_attribute(attribute_dict)
+
+        first_node_by_o_set = self.classification_by_attribute(
+            data_set, attribute_position, 'o')
+        attribute_dict = self.get_each_attribute(first_node_by_o_set, len(self.attributes))
+        attribute_position_3, second_node_3 = self.choose_attribute(attribute_dict)
+
+        # 计算第三层的节点
+        second_node_1_by_x_set = self.classification_by_attribute(
+            first_node_by_x_set, attribute_position_1, 'x')
+        attribute_dict = self.get_each_attribute(second_node_1_by_x_set, len(self.attributes))
+        attribute_position_4, third_node_1 = self.choose_attribute(attribute_dict)
+
+        second_node_1_by_o_set = self.classification_by_attribute(
+            first_node_by_x_set, attribute_position_1, 'o')
+        attribute_dict = self.get_each_attribute(second_node_1_by_o_set, len(self.attributes))
+        attribute_position_5, third_node_2 = self.choose_attribute(attribute_dict)
+
+        second_node_1_by_b_set = self.classification_by_attribute(
+            first_node_by_x_set, attribute_position_1, 'b')
+        attribute_dict = self.get_each_attribute(second_node_1_by_b_set, len(self.attributes))
+        attribute_position_6, third_node_3 = self.choose_attribute(attribute_dict)
+
+        second_node_2_by_x_set = self.classification_by_attribute(
+            first_node_by_b_set, attribute_position_2, 'x')
+        attribute_dict = self.get_each_attribute(second_node_2_by_x_set, len(self.attributes))
+        attribute_position_7, third_node_4 = self.choose_attribute(attribute_dict)
+
+        second_node_2_by_b_set = self.classification_by_attribute(
+            first_node_by_b_set, attribute_position_2, 'b')
+        attribute_dict = self.get_each_attribute(second_node_2_by_b_set, len(self.attributes))
+        attribute_position_8, third_node_5 = self.choose_attribute(attribute_dict)
+
 
 de = DecisionTree()
-de.get_each_attribute()
-print(de.get_root_node())
+print(de.get_tree())
